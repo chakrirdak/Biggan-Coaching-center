27f263b0c33b3ce4d6f506dc81a3bc1d60d8bc81
chakrirdak <chakrirdak74@gmail.com>
2025-11-18 19:46:49 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,7 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
+
+tasks.register<Wrapper>("wrapper") {
+    gradleVersion = "8.0"
+}
